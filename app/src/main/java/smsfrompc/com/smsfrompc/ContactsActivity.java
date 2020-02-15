b60a8160ec0404c0183199c08df057619a3b6a16
Valentinas Kasteckis <41127259+Tortonas@users.noreply.github.com>
2020-02-15 15:15:23 +0200

    Custom List View Adapter to show Contacts

--- app/src/main/java/smsfrompc/com/smsfrompc/ContactsActivity.java
+++ app/src/main/java/smsfrompc/com/smsfrompc/ContactsActivity.java
@@ -3,6 +3,10 @@ package smsfrompc.com.smsfrompc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ContactsActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class ContactsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
+        ListView contactsListView = findViewById(R.id.contactsListView);
+        Contact a = new Contact("random", "random");
+        Contact b = new Contact("random2", "random");
+        Contact c = new Contact("random3", "randomNUMBER3");
+
+        ArrayList<Contact> contactList = new ArrayList<>();
+        contactList.add(a);
+        contactList.add(b);
+        contactList.add(c);
+
+        ContactsListAdapter adapter = new ContactsListAdapter(this, R.layout.contacts_list_view, contactList);
+        contactsListView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/smsfrompc/com/smsfrompc/ContactsListAdapter.java
@@ -0,0 +1,66 @@
+package smsfrompc.com.smsfrompc;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class ContactsListAdapter extends ArrayAdapter<Contact> {
+
+    private Context mContext;
+    int mResource;
+
+    public ContactsListAdapter(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
+
+    public ContactsListAdapter(@NonNull Context context, int resource, int textViewResourceId) {
+        super(context, resource, textViewResourceId);
+    }
+
+    public ContactsListAdapter(@NonNull Context context, int resource, @NonNull Contact[] objects) {
+        super(context, resource, objects);
+    }
+
+    public ContactsListAdapter(@NonNull Context context, int resource, int textViewResourceId, @NonNull Contact[] objects) {
+        super(context, resource, textViewResourceId, objects);
+    }
+
+    public ContactsListAdapter(@NonNull Context context, int resource, @NonNull List<Contact> objects) {
+        super(context, resource, objects);
+        mContext = context;
+        mResource = resource;
+    }
+
+    public ContactsListAdapter(@NonNull Context context, int resource, int textViewResourceId, @NonNull List<Contact> objects) {
+        super(context, resource, textViewResourceId, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        String name = getItem(position).getName();
+        String number = getItem(position).getNumber();
+
+        Contact contact = new Contact(name, number);
+
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+
+        convertView = inflater.inflate(mResource, parent, false);
+
+        TextView tvName = convertView.findViewById(R.id.nameTextView);
+        TextView tvNumber = convertView.findViewById(R.id.numberTextView);
+
+        tvName.setText(name);
+        tvName.setText(number);
+
+        return convertView;
+    }
+}
