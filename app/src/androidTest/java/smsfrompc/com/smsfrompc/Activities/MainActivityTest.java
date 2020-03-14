a3abe69fd3430211e88945a4edeece16501e09f4
Valentinas Kasteckis <41127259+Tortonas@users.noreply.github.com>
2020-03-14 15:01:26 +0200

    Added testing rule dependency for unit testing and created one failing unit test

---
+++ app/src/androidTest/java/smsfrompc/com/smsfrompc/Activities/MainActivityTest.java
@@ -0,0 +1,37 @@
+package smsfrompc.com.smsfrompc.Activities;
+
+import android.view.View;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+
+import smsfrompc.com.smsfrompc.R;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    private MainActivity mActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = mainActivityActivityTestRule.getActivity();
+    }
+
+    public void testLaunch()
+    {
+        View view = mActivity.findViewById(R.id.delaySmsBtn5);
+        assertNotNull(view);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mActivity = null;
+    }
+}
